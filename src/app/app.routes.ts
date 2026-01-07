344ff2a7a705cae7d05ec6cb7b7744b55889c7df
Jeffson Sucaldito <jcsucaldito17762@liceo.edu.ph>
2026-01-07 13:01:48 +0800

    Prelim-exam submission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { prelimExam } from './prelim-exam/prelim-exam.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'prelim-exam',
+        component: prelimExam
+    }
+];
